d30d5180fca95f4b493e944c3030489bc0bbd4bf
KyleRBoldman <32073292+KyleRBoldman@users.noreply.github.com>
2017-10-05 13:35:41 -0400

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace ChangingHealthWithTimer
+{
+    partial class MadDuckFight
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.attackTimer = new System.Windows.Forms.Timer(this.components);
+            this.healthLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::ChangingHealthWithTimer.Properties.Resources.enemy;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(155, 96);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(99, 106);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // attackTimer
+            // 
+            this.attackTimer.Interval = 500;
+            // 
+            // healthLabel
+            // 
+            this.healthLabel.AutoSize = true;
+            this.healthLabel.BackColor = System.Drawing.SystemColors.Menu;
+            this.healthLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.healthLabel.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.healthLabel.ForeColor = System.Drawing.Color.Fuchsia;
+            this.healthLabel.Location = new System.Drawing.Point(129, 297);
+            this.healthLabel.Name = "healthLabel";
+            this.healthLabel.Size = new System.Drawing.Size(2, 25);
+            this.healthLabel.TabIndex = 1;
+            this.healthLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // MadDuckFight
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ChangingHealthWithTimer.Properties.Resources.background;
+            this.ClientSize = new System.Drawing.Size(408, 363);
+            this.Controls.Add(this.healthLabel);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "MadDuckFight";
+            this.Text = "Mad Duck Fight";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer attackTimer;
+        private System.Windows.Forms.Label healthLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChangingHealthWithTimer
+{
+    public partial class MadDuckFight : Form
+    {
+        int health = 100;
+
+        public MadDuckFight()
+        {
+            InitializeComponent();
+            do
+            {
+                healthLabel.Text = "Health = " + health;
+                attackTimer.Start();
+                if (attackTimer.Equals(0))
+                {
+                    health -= 5;
+                }
+            } while (health > 0);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
